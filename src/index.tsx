c7eb5e2c613eb6ceb59cf47b81d20482117803b9
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2017-03-16 19:55:45 +0000

    use store

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 // tslint:disable-next-line:no-unused-variable
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
 import { browserHistory, Router } from 'react-router';
+import initialState from './core/reducers/initialState';
+import configureStore from './core/store/configureStore';
 import routes from './routes';
 
+const store = configureStore(initialState);
+
 ReactDom.render(
-    <Router routes={routes} history={browserHistory} />,
+    <Provider store={store}>
+        <Router routes={routes} history={browserHistory} />
+    </Provider>,
     document.getElementById('app')
 );
